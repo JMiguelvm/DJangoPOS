4df602e82436d39c12b4a3ce6faac4692c3b1b7a
JMiguelvm <mylogan360@gmail.com>
2025-03-10 22:42:14 -0500

    Editado modelo de stock, diseño de edit y create en stock.

--- core/stock/models.py
+++ core/stock/models.py
@@ -4,8 +4,14 @@ from products.models import Product
 
 class ProductStock(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    stock = models.IntegerField(default=0)
+    def __str__(self):
+        return f"{self.product.name} - {self.stock}"
+
+class StockItem(models.Model):
+    product_stock = models.ForeignKey(ProductStock, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
     date = models.DateTimeField(default=timezone.now)
     buy_price = models.IntegerField(default=0)
     def __str__(self):
-        return f"{self.product.name} - {self.quantity}"
+        return f"{self.product_stock.id} - {self.quantity}"
