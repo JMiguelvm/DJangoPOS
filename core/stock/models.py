07e43077e4fe8ee7fd8f9eda5d33df3cc4ece22a
JMiguelvm <mylogan360@gmail.com>
2025-06-14 12:15:45 -0500

    Cambiado metodo total stock de product stock, para que recalcule el inventario de si mismo al llamarse.

--- core/stock/models.py
+++ core/stock/models.py
@@ -6,15 +6,23 @@ class ProductStock(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     stock = models.IntegerField(default=0)
     def __str__(self):
-        return f"{self.product.name} - {self.product.id}- {self.stock}"
+        return f"{self.product.name} - ID PS: {self.id}- {self.product.id}- {self.stock}"
     def total_stock(self):
-        return sum(item.quantity for item in self.stockitem_set.filter(voided=False))
+        total = sum(
+            item.quantity
+            for item in self.stockitem_set.filter(voided=False)
+        )
+        self.stock = total
+        self.save(update_fields=['stock'])
+        return total
 
 class StockItem(models.Model):
     product_stock = models.ForeignKey(ProductStock, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
+    quantity_used = models.PositiveIntegerField(default=0)
     date = models.DateTimeField(default=timezone.now)
     buy_price = models.IntegerField(default=0) # Whitout taxes
     voided = models.BooleanField(default=False)
+    is_depleted = models.BooleanField(default=False)
     def __str__(self):
         return f"{self.product_stock.id} - {self.quantity}"
