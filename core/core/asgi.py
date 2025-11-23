25c8ca0a6f4e5391c8a4fcc19157548adf53e102
JMiguelvm <mylogan360@gmail.com>
2025-11-23 16:03:46 -0500

    Prueba sin websockets

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -1,9 +1,15 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from . import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Usa solo HTTP por ahora
+application = get_asgi_application()
+
+# Deshabilitado websocket
+"""
+from channels.routing import ProtocolTypeRouter, URLRouter
+from . import routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -11,3 +17,4 @@ application = ProtocolTypeRouter({
         routing.websocket_urlpatterns
     ),
 })
+"""
